0cdf686badd88176d70a9ec80d088d891067eae4
Siarhei VASILIUK <102259012+Serg56V@users.noreply.github.com>
2022-12-25 14:21:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+new WOW().init();
+
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.projects-pagination',
+    bulletClass: 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true
+  },
+});
+
